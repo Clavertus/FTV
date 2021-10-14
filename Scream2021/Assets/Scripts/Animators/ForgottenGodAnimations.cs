f4a23c73d56685c713466c3e73276debbcfb4af7
Azkabal <kravchyshynk@ukr.net>
2021-10-14 09:49:56 +0200

    Added Forgotten God Test Scene
    Added forgotten God model and animations

---
+++ Scream2021/Assets/Scripts/Animators/ForgottenGodAnimations.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForgottenGodAnimations : MonoBehaviour
+{
+    [SerializeField] Animator animator = null;
+    public void PlayIdle()
+    {
+        animator.SetTrigger("Idle");
+    }
+    public void PlayOpen()
+    {
+        animator.SetTrigger("Open");
+    }
+    public void PlayClose()
+    {
+        animator.SetTrigger("Close");
+    }
+}
