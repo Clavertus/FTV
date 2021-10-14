374f1ba0725e710ff380bdf7e37352b8d2c23c12
Azkabal <kravchyshynk@ukr.net>
2021-10-14 09:17:57 +0200

    Added Test Scene with Train Creature Prefab on it and animations buttons
    Added Test Scene with Train Creature Prefab on it and animations buttons

---
+++ Scream2021/Assets/Scripts/TrainCreatureAnimationController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainCreatureAnimationController : MonoBehaviour
+{
+    [SerializeField] Animator animator = null;
+
+    public void PlayIdle()
+    {
+        animator.SetTrigger("Idle");
+    }
+    public void PlayWalk()
+    {
+        animator.SetTrigger("Walk");
+    }
+    public void PlayRun()
+    {
+        animator.SetTrigger("Run");
+    }
+    public void PlayIdleToActive()
+    {
+        animator.SetTrigger("IdleToActive");
+    }
+    public void PlayIdleActive()
+    {
+        animator.SetTrigger("IdleActive");
+    }
+    public void PlayRunActive()
+    {
+        animator.SetTrigger("RunActive");
+    }
+    public void PlayOpenDoorActive()
+    {
+        animator.SetTrigger("OpenDoorFullActive");
+    }
+    public void PlayOpenDoorInactive()
+    {
+        animator.SetTrigger("OpenDoorFullInactive");
+    }
+    public void PlayRunToJump()
+    {
+        animator.SetTrigger("RunToJump");
+    }
+    public void PlayInJump()
+    {
+        animator.SetTrigger("InJump");
+    }
+}
