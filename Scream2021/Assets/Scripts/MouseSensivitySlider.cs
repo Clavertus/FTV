6d55809f74f544aaa842c12998bb1d532291b819
Azkabal <kravchyshynk@ukr.net>
2021-11-03 07:08:22 +0100

    Saving mouse sensivity in PlayerPrefs

--- Scream2021/Assets/Scripts/MouseSensivitySlider.cs
+++ Scream2021/Assets/Scripts/MouseSensivitySlider.cs
@@ -14,11 +14,17 @@ public class MouseSensivitySlider : MonoBehaviour
 	{
 		if(sensivitySlider) sensivitySlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
 
+		if (PlayerPrefs.HasKey("mouse_sensivity")) sensivitySlider.value = PlayerPrefs.GetFloat("mouse_sensivity");
+
 		mouseLook = FindObjectOfType<MouseLook>();
 		if(mouseLook)
 		{
-			Debug.Log("Found!");
+			//Debug.Log("Found!");
 			if (mouseLook) mouseLook.mouseSensitivity = sensivitySlider.value;
+		}
+		else
+		{
+			Debug.LogError("Mouse Look component requirent somewhere on scene to apply mouse sensivity");
 		}
 	}
 
@@ -26,5 +32,6 @@ public class MouseSensivitySlider : MonoBehaviour
 	{
 		Debug.Log(sensivitySlider.value);
 		if(mouseLook) mouseLook.mouseSensitivity = sensivitySlider.value;
+		PlayerPrefs.SetFloat("mouse_sensivity", sensivitySlider.value);
 	}
 }
