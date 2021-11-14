f5a127af466b6bafef753fccd566a19926482b44
Azkabal <kravchyshynk@ukr.net>
2021-11-14 17:31:39 +0100

    children of menu enabled on the start to make all start scripts of them work

--- Scream2021/Assets/Scripts/UI/InGameMenuCotrols.cs
+++ Scream2021/Assets/Scripts/UI/InGameMenuCotrols.cs
@@ -7,6 +7,7 @@ public class InGameMenuCotrols : MonoBehaviour
     bool menuActive = false;
 
     [SerializeField] GameObject[] children = null;
+
     // Start is called before the first frame update
     void Start()
     {
