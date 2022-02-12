a078bda4a356791aef28fb2bc636af9e958a1ac0
Azkabal <kravchyshynk@ukr.net>
2022-02-12 18:58:38 +0100

    Saving on entry to Tara's Scene

--- Scream2021/Assets/Scripts/TaraEpisodeManager.cs
+++ Scream2021/Assets/Scripts/TaraEpisodeManager.cs
@@ -1,10 +1,12 @@
+using FTV.Saving;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TaraEpisodeManager : MonoBehaviour
+public class TaraEpisodeManager : MonoBehaviour, ISaveable
 {
     private PlayerMovement player = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,30 @@ public class TaraEpisodeManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(saved_on_entry == false)
+        {
+            saved_on_entry = true;
+            FindObjectOfType<SavingWrapper>().CheckpointSave();
+        }
+    }
+
+    bool saved_on_entry = false;
+    [System.Serializable]
+    struct SaveData
+    {
+        public bool saved_on_entry;
+    }
+
+    public object CaptureState()
+    {
+        SaveData data = new SaveData();
+        data.saved_on_entry = saved_on_entry;
+        return data;
+    }
+
+    public void RestoreState(object state)
+    {
+        SaveData data = (SaveData)state;
+        saved_on_entry = data.saved_on_entry;
     }
 }
