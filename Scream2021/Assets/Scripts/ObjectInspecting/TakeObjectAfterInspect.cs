9fd77da6a9a51c030984905d32b14af51c09e12d
Azkabal <kravchyshynk@ukr.net>
2022-05-09 13:50:54 +0200

    Added Tara Notes and Tara Red Eyes...

--- Scream2021/Assets/Scripts/ObjectInspecting/TakeObjectAfterInspect.cs
+++ Scream2021/Assets/Scripts/ObjectInspecting/TakeObjectAfterInspect.cs
@@ -7,8 +7,21 @@ public class TakeObjectAfterInspect : MonoBehaviour
     [SerializeField] bool takeObjectToInventory = false;
     [SerializeField] GameObject hideObject = null;
     [SerializeField] GameObject revealObject = null;
+    [SerializeField] bool delayEnable = false;
+    [SerializeField] float delayTime = 0.15f;
 
     public int interactionCounter = 0;
+    float delayCounter = 0.0f;
+
+    private void OnEnable()
+    {
+        if(delayEnable)
+        {
+            delayCounter = 0.0f;
+            GetComponent<Selectable>().enabled = false;
+            GetComponent<BoxCollider>().enabled = false;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +31,21 @@ public class TakeObjectAfterInspect : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(delayEnable)
+        {
+            if(delayCounter > delayTime)
+            {
+                delayEnable = false;
+                GetComponent<Selectable>().enabled = true;
+                GetComponent<BoxCollider>().enabled = true;
+            }
+            else
+            {
+                delayCounter += Time.deltaTime;
+            }
+            return;
+        }
+
         if (takeObjectToInventory == false) return;
 
         if (gameObject.tag == "Selected" && interactionCounter == 0) { Interaction(); return; }
@@ -26,7 +54,7 @@ public class TakeObjectAfterInspect : MonoBehaviour
     private void Interaction()
     {
         interactionCounter++;
-        revealObject.SetActive(true);
-        hideObject.SetActive(false);
+        if (revealObject) revealObject.SetActive(true);
+        if(hideObject) hideObject.SetActive(false);
     }
 }
