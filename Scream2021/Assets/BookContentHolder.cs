3b410a4c5e8c9ca37fc87ad7d99c51a333a9cb2c
Azkabal <kravchyshynk@ukr.net>
2022-05-21 17:51:45 +0200

    More work on the book code

--- Scream2021/Assets/BookContentHolder.cs
+++ Scream2021/Assets/BookContentHolder.cs
@@ -9,6 +9,8 @@ public class BookContentHolder : MonoBehaviour
 {
     enum page { left, right };
 
+    [SerializeField] CanvasGroup ChangePageUiCanvasLeft = null;
+    [SerializeField] CanvasGroup ChangePageUiCanvasRight = null;
     [SerializeField] CanvasGroup pageLeft = null;
     [SerializeField] CanvasGroup pageRight = null;
 
@@ -20,15 +22,29 @@ public class BookContentHolder : MonoBehaviour
         public Color pageImageColor;
     };
 
-
     [SerializeField] pageContent[] bookContent = null;
 
     Image[] pageImage = null;
 
     TMP_Text[] pageText = null;
 
+    int[] currentPageContent = null;
+
     private void Start()
     {
+        ChangePageUiCanvasLeft.alpha = 0;
+        ChangePageUiCanvasRight.alpha = 0;
+
+        if (bookContent.Length % 2 != 0)
+        {
+            Debug.LogError("Book content should be dividable through 2");
+            return;
+        }
+        currentPageContent = new int[2];
+
+        currentPageContent[0] = 0;
+        currentPageContent[1] = 1;
+
         pageImage = new Image[2];
         pageText = new TMP_Text[2];
 
@@ -42,11 +58,96 @@ public class BookContentHolder : MonoBehaviour
         FillPageWithContent(page.right, bookContent[1]);
     }
 
+    public void ShowUI()
+    {
+        if(bookContent.Length <= 2)
+        {
+            return;
+        }
+
+        ChangePageUiCanvasRight.alpha = 1;
+        ChangePageUiCanvasLeft.alpha = 0;
+    }
+    public void HideUI()
+    {
+        ChangePageUiCanvasRight.alpha = 0;
+        ChangePageUiCanvasLeft.alpha = 0;
+    }
+
+    public void ResetPages()
+    {
+        int nextIntForLeftPage = 0;
+        int nextIntForRightPage = 1;
+        FillPageWithContent(page.left, bookContent[nextIntForLeftPage]);
+        currentPageContent[(int)page.left] = nextIntForLeftPage;
+
+        FillPageWithContent(page.right, bookContent[nextIntForRightPage]);
+        currentPageContent[(int)page.right] = nextIntForRightPage;
+    }
+
+    public int GetNumberOfPages()
+    {
+        return bookContent.Length;
+    }
+
     private void FillPageWithContent(page pageId, pageContent pageContent)
     {
         int pageIntId = (int) pageId;
         pageImage[pageIntId].sprite = pageContent.pageImageSprite;
         pageImage[pageIntId].color = pageContent.pageImageColor;
         pageText[pageIntId].text = pageContent.pageText;
+    }
+
+    public void FillPagesWithNextContentRightDirection()
+    {
+        if (bookContent.Length > 2)
+        {
+            ChangePageUiCanvasLeft.alpha = 1;
+        }
+        int nextIntForLeftPage = currentPageContent[(int)page.left] + 2;
+        int nextIntForRightPage = currentPageContent[(int)page.right] + 2;
+
+        if (nextIntForLeftPage >= bookContent.Length)
+        {
+            nextIntForLeftPage = bookContent.Length - 2;
+            nextIntForRightPage = bookContent.Length - 1;
+            ChangePageUiCanvasRight.alpha = 0;
+        }
+        else if (nextIntForLeftPage == bookContent.Length -2)
+        {
+            ChangePageUiCanvasRight.alpha = 0;
+        }
+
+        FillPageWithContent(page.left, bookContent[nextIntForLeftPage]);
+        currentPageContent[(int)page.left] = nextIntForLeftPage;
+
+        FillPageWithContent(page.right, bookContent[nextIntForRightPage]);
+        currentPageContent[(int)page.right] = nextIntForRightPage;
+    }
+    public void FillPagesWithNextContentLeftDirection()
+    {
+        if (bookContent.Length > 2)
+        {
+            ChangePageUiCanvasRight.alpha = 1;
+        }
+        int nextIntForLeftPage = currentPageContent[(int)page.left] - 2;
+        int nextIntForRightPage = currentPageContent[(int)page.right] - 2;
+
+        if (nextIntForLeftPage < 0)
+        {
+            nextIntForLeftPage = 0;
+            nextIntForRightPage = 1;
+            ChangePageUiCanvasLeft.alpha = 0;
+        }
+        else if(nextIntForLeftPage == 0)
+        {
+            ChangePageUiCanvasLeft.alpha = 0;
+        }
+
+        FillPageWithContent(page.left, bookContent[nextIntForLeftPage]);
+        currentPageContent[(int)page.left] = nextIntForLeftPage;
+
+        FillPageWithContent(page.right, bookContent[nextIntForRightPage]);
+        currentPageContent[(int)page.right] = nextIntForRightPage;
     }
 }
---
+++ Scream2021/Assets/BookReader.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookReader : MonoBehaviour
+{
+    [SerializeField] Animator bookAnimator = null;
+    [SerializeField] BookContentHolder bookContent = null;
+
+    private int interactionCounter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactionCounter = 0;
+        bookAnimator.SetTrigger("Closed");
+    }
+
+    private void Update()
+    {
+        if (gameObject.tag == "Selected" && (interactionCounter == 0)) { StartCoroutine(Interaction()); return; }
+    }
+
+    private IEnumerator Interaction()
+    {
+        gameObject.tag = "Untagged";
+        interactionCounter++;
+
+        FindObjectOfType<PlayerMovement>().LockPlayer();
+        FindObjectOfType<MouseLook>().LockCamera();
+        FindObjectOfType<InGameMenuCotrols>().LockMenuControl();
+
+        OpenBook();
+
+        bool exit = false;
+        while (exit == false)
+        {
+            yield return new WaitUntil(() => (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A)));
+            {
+                if(Input.GetKeyDown(KeyCode.Escape))
+                {
+                    exit = true;
+                }
+                else if(Input.GetKeyDown(KeyCode.A))
+                {
+                    ListBookLeft(); yield return new WaitForSeconds(0.1f);
+                }
+                else if(Input.GetKeyDown(KeyCode.D))
+                {
+                    ListBookRight(); yield return new WaitForSeconds(0.1f);
+                }
+            }
+        }
+
+
+        FindObjectOfType<PlayerMovement>().UnlockPlayer();
+        FindObjectOfType<MouseLook>().UnlockCamera();
+        FindObjectOfType<InGameMenuCotrols>().UnlockMenuControl();
+
+        CloseBook();
+
+        interactionCounter--;
+        gameObject.tag = "Selectable";
+    }
+
+    public void OpenBook()
+    {
+        bookAnimator.SetTrigger("Open");
+        bookContent.ResetPages();
+        bookContent.ShowUI();
+    }
+    public void CloseBook()
+    {
+        bookAnimator.SetTrigger("Close");
+        bookContent.HideUI();
+    }
+
+    public void ListBookRight()
+    {
+        bookContent.FillPagesWithNextContentRightDirection();
+    }
+    public void ListBookLeft()
+    {
+        bookContent.FillPagesWithNextContentLeftDirection();
+    }
+}
