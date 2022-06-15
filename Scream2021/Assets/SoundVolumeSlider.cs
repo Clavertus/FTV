99b19ad50a3e73a9f9c0a3cd3f59eead49605845
Rafał Piechnik <clavertus@interia.pl>
2022-06-16 00:11:49 +0200

    Sound Control Finalized

--- Scream2021/Assets/SoundVolumeSlider.cs
+++ Scream2021/Assets/SoundVolumeSlider.cs
@@ -9,11 +9,32 @@ public class SoundVolumeSlider : MonoBehaviour
     public Slider soundSlider;
     public AudioMixer am;
 
+     public float defaultVol = 0.3f;
+
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("masterVol"))
+        {
+            soundSlider.value = PlayerPrefs.GetFloat("masterVol");
+            ValueChange();
+        } else
+        {           
+            PlayerPrefs.SetFloat("masterVol", defaultVol);
+            soundSlider.value = defaultVol;
+            ValueChange();
+        }
+    }
+
+
 
     public void ValueChange()
     {
+        
         float sliderVal = soundSlider.value;
         am.SetFloat("masterVol", 20*Mathf.Log10(sliderVal));
+        PlayerPrefs.SetFloat("masterVol", sliderVal);
+
     }
 
 }
